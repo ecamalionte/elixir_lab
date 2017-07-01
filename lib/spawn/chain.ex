658ec62fd04652575e03fc4232e758b5b9eed707
Eric Camalionte <eric80hz@yahoo.com.br>
2017-07-01 20:49:51 -0300

    Add Chain example

---
+++ lib/spawn/chain.ex
@@ -0,0 +1,28 @@
+defmodule Chain do
+  def counter(next_pid) do
+    receive do
+      n ->
+        IO.inspect next_pid
+        send next_pid, n + 1
+    end
+  end
+
+  def create_process(n) do
+    last = Enum.reduce 1..n, self(),
+    fn (_, next_pid) ->
+      spawn(Chain, :counter, [next_pid])
+    end
+
+    send last, 0 #start the count by sending a zero to the last process
+
+    receive do # and wait for the result to come back to us
+      result when is_integer(result) -> "Result is #{result}"
+    end
+  end
+
+  def run(n) do
+   IO.puts inspect :timer.tc(Chain, :create_process,[n])
+  end
+end
+
+Chain.run(10)
